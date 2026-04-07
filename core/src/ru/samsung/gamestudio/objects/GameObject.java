7398bceb8763063567e736f81f031cccc19d8bce
oooarem-hash <oooarem@gmail.com>
2026-04-07 18:11:59 +0300

    1

--- core/src/ru/samsung/gamestudio/objects/GameObject.java
+++ core/src/ru/samsung/gamestudio/objects/GameObject.java
@@ -1,5 +1,7 @@
 package ru.samsung.gamestudio.objects;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -9,6 +11,20 @@ import com.badlogic.gdx.physics.box2d.World;
 import ru.samsung.gamestudio.GameSettings;
 
 public class GameObject {
+    Texture texture;
+    Body body;
+
+    GameObject(String texturePath, int x, int y, int width, int height, World world) {
+        this.width = width;
+        this.height = height;
+
+        texture = new Texture(texturePath);
+        body = createBody(x, y, world);
+    }
+    public void draw(SpriteBatch batch) {
+        batch.draw(texture, getX() - (width / 2f), getY() - (height / 2f), width, height);
+    }
+
     private Body createBody(float x, float y, World world) {
         BodyDef def = new BodyDef(); // def - defenition (определение) это объект, который содержит все данные, необходимые для посторения тела
 
@@ -29,5 +45,20 @@ public class GameObject {
 
         body.setTransform(x * GameSettings.SCALE, y * GameSettings.SCALE, 0); // устанавливаем позицию тела по координатным осям и угол поворота
         return body;
+    }
+    public int getX() {
+        return (int) (body.getPosition().x / GameSettings.SCALE);
+    }
+
+    public int getY() {
+        return (int) (body.getPosition().y / GameSettings.SCALE);
+    }
+
+    public void setX(int x) {
+        body.setTransform(x * GameSettings.SCALE, body.getPosition().y, 0);
+    }
+
+    public void setY(int y) {
+        body.setTransform(body.getPosition().x, y * GameSettings.SCALE, 0);
     }
 }
